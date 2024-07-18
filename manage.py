393d9ce46485f09890947cdcd0a8bcdaf010e2a0
logesh-works <logeshkumar.dev@gmail.com>
2024-07-18 23:53:29 +0530

    v2.1

--- manage.py
+++ manage.py
@@ -5,7 +5,13 @@ import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "csc_app.settings")
+
+    # Override default port for `runserver` command
+    from django.core.management.commands.runserver import Command as runserver
+    runserver.default_port = "30001"
+    
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'csc_app.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,5 +23,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
