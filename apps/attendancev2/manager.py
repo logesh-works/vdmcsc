4975892a2dc3930ceee2acd6914a32c5acd8a249
logesh-works <logeshkumar.dev@gmail.com>
2024-07-19 22:02:57 +0530

    v2.1

--- apps/attendancev2/manager.py
+++ apps/attendancev2/manager.py
@@ -5,14 +5,6 @@ class AttendanceManager:
     def __init__(self,mongodb_database):
         self.db_name = mongodb_database
         self.client = pymongo.MongoClient("mongodb://admin2_cscadmin:Cscadmin123@localhost:27017/admin2_vdm?authSource=admin")
-        self.db = self.client[self.db_name]
-        self.db.command(
-            "updateUser",
-            "admin2_cscadmin",
-            roles=[
-                {"role": "readWrite", "db": f"{self.db_name}"}
-            ]
-        )
         self.staff_collection = self.db['staff_collection']
         self.student_collection = self.db['student_collection']
         self.lab_collection = self.db['lab_collection']
